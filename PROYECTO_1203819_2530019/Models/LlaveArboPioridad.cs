aa9a18e9d147a4bf2c48cd0266458da544c7307d
DianaAragon <78452952+DianaAragon@users.noreply.github.com>
2021-04-29 15:25:48 -0600

    Update LlaveArboPioridad.cs

--- PROYECTO_1203819_2530019/Models/LlaveArboPioridad.cs
+++ PROYECTO_1203819_2530019/Models/LlaveArboPioridad.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,11 +7,7 @@ namespace PROYECTO_1203819_2530019.Models
 {
     public class LlaveArbolPrioridad
     {
-<<<<<<< Updated upstream
-        public int CodigoHash { get; set; }
-=======
         public Int64 CodigoHash { get; set; }
->>>>>>> Stashed changes
         public double Prioridad { get; set; }
         public static int Compare_Llave_Arbol(LlaveArbolPrioridad x, LlaveArbolPrioridad y)
         {
