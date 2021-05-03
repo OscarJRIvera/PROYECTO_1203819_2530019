206691788d881f3807ff3cc28049a36cf3486d4e
DianaAragon <daragonl17d@gmail.com>
2021-05-03 16:21:21 -0600

    vistas, porcentajes y pacientes  vacunar

--- PROYECTO_1203819_2530019/Models/PacienteView.cs
+++ PROYECTO_1203819_2530019/Models/PacienteView.cs
@@ -10,11 +10,9 @@ namespace PROYECTO_1203819_2530019.Models
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public Int64 DPI { get; set; }
+        public int DPI { get; set; }
         public string Departamento { get; set; }
         public string Municipio { get; set; }
         public int Edad { get; set; }
-        public double Prioridad { get; set; }
-        public DateTime Fecha { get; set; }
     }
 }
---
+++ PROYECTO_1203819_2530019/Models/Porcentajes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROYECTO_1203819_2530019.Models
+{
+    public class Porcentajes
+    {
+        public int Id { get; set; }
+        public int Total { get; set; }
+        public int Vacunados { get; set; }
+        public string porcentaje { get; set; }
+
+    }
+}
